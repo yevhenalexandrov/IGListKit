b17855f76498df377ed8822bc60706bc133f3993
Yevhen Alexandrov <zhenya.bobyr@icloud.com>
2020-12-12 23:50:17 +0200

    Working to enable SPM support for IGListKit.

--- Package.swift
+++ Package.swift
@@ -1,15 +1,36 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "IGListKit",
     products: [
-        .library(name: "IGListKit", targets: ["IGListKit"])
+        .library(
+            name: "IGListKit", 
+            targets: [ "IGListDiffKit", "IGListKit", "IGListSwiftKit"]
+        )
     ],
     targets: [
         .target(
+            name: "IGListDiffKit",
+            path: "Source/IGListDiffKit",
+            publicHeadersPath: ".",
+            cSettings:[
+                .headerSearchPath("Internal"),
+            ]
+        ),
+        .target(
             name: "IGListKit",
-            path: "Source"
+            dependencies:[
+                "IGListDiffKit"
+            ],
+            path: "Source/IGListKit",
+            cSettings:[
+                .headerSearchPath("Internal"),
+            ]
+        ),
+        .target(
+            name: "IGListSwiftKit",
+            path: "Source/IGListSwiftKit"
         )
     ]
 )
